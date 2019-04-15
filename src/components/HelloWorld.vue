1b37a69a58c676c062798ae58c133ed924a43d4a
Gheorghe Madalina Eleonora <madalinaeleonora.gheorghe@gmail.com>
2019-04-15 17:21:12 +0300

    solved lifecycle and progress responses

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,11 +4,24 @@
       <v-flex xs12 sm12>
         <v-progress-linear color="blue" height="20" :value="actualChapter.id *10"></v-progress-linear>
         <v-card color="white">
+          <div class="text-xs-center" v-if="actualChapter.id === '11'">
+            <v-card-text
+              class="text-xs-center"
+            >{{ ((correctQuestions * 100)/allQuestions) >= 80 ? 'Felicitari!' : 'Mai incearca!'}}</v-card-text>
+            <v-progress-circular
+              :rotate="360"
+              :size="100"
+              :width="15"
+              :value="(correctQuestions * 100)/allQuestions"
+              :color="(correctQuestions * 100)/allQuestions >= 80 ? 'green' : 'red'"
+            >{{ Math.round(((correctQuestions * 100)/allQuestions) * 100) / 100 }}</v-progress-circular>
+            <v-card-text>
+            <v-btn flat color="blue" @click="tryAgain()">Incearca din nou</v-btn>
+            </v-card-text>
+          </div>
           <v-card-title class="blue--text font-weight-bold">{{actualChapter.title}}</v-card-title>
           <v-card-text class="black--text" v-html="breakIt(actualChapter.theory)"></v-card-text>
-          <div
-            v-if="(actualChapter.id != '10') && (actualChapter.id != '9') && (actualChapter.id != '1')"
-          >
+          <div v-if="actualChapter.questions">
             <v-card-text v-if="!questionShow">
               <v-btn @click="questionShow = !questionShow">Arata intrebarile!</v-btn>
             </v-card-text>
@@ -21,6 +34,7 @@
                     :value="index+'-'+index2"
                     :color="setShowResponses(index, index2)"
                     :readonly="disabledSelect"
+                    @change="verifyIfCorrect(index, index2)"
                     v-for="(answer, index2) in question.answers"
                     :key="index2"
                   ></v-radio>
@@ -28,15 +42,18 @@
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn @click="seeAnswers()" v-if="seeAnswersVar === false && answersSeen === false">Vezi raspunsuri</v-btn>
+                <v-btn
+                  @click="seeAnswers()"
+                  v-if="seeAnswersVar === false && answersSeen === false"
+                >Vezi raspunsuri</v-btn>
               </v-card-actions>
             </v-card-text>
           </div>
           <v-card-actions
-            v-if="(actualChapter.id === '10') || (actualChapter.id === '9') || (actualChapter.id === '1') || (answersSeen === true)"
+            v-if="(!actualChapter.questions && (actualChapter.id != '11'))  || (answersSeen === true)"
           >
-          <v-spacer></v-spacer>
-              <v-btn @click="nextQuestion()">Urmatoarea lectie</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn @click="nextChapter()">Urmatoarea lectie</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -57,7 +74,9 @@ export default {
     disabledSelect: false,
     answers: [],
     seeAnswersVar: false,
-    answersSeen: false
+    answersSeen: false,
+    allQuestions: 10,
+    correctQuestions: 8
   }),
   created() {
     this.database = db.default.chapters;
@@ -68,7 +87,7 @@ export default {
       this.questionShow = index;
     },
     breakIt(value) {
-      if (this.actualChapter.id === "10") {
+      if (this.actualChapter.id === "11") {
         return value;
       } else {
         let subtitleCheckForBr1 = value.split("<i>").join("<br><i>");
@@ -80,12 +99,16 @@ export default {
         return brCheck;
       }
     },
-    nextQuestion() {
-      if (this.database.length === this.chapterNumber + 1) {
-        this.chapterNumber = 0;
-      } else {
-        this.chapterNumber = this.chapterNumber + 1;
+    tryAgain() {
+      this.chapterNumber = 0
+      this.actualChapter = this.database[this.chapterNumber];
+    },
+    nextChapter() {
+      if (this.actualChapter.questions) {
+        this.allQuestions =
+          this.allQuestions + this.actualChapter.questions.length;
       }
+      this.chapterNumber = this.chapterNumber + 1;
       this.actualChapter = this.database[this.chapterNumber];
       this.questionShow = false;
       this.radios = null;
@@ -94,30 +117,45 @@ export default {
       this.disabledSelect = false;
       this.answersSeen = false;
     },
+    verifyIfCorrect(index, index2) {
+      if (
+        this.actualChapter.questions[index].answers[index2].correct === true
+      ) {
+        this.correctQuestions = this.correctQuestions + 1;
+      }
+    },
+    verifyAnswers(index, index2) {
+      if (
+        this.actualChapter.questions[index].answers[index2].correct === true
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
     /*
     index = index intrebare
     index2 = index raspuns
     */
-    verifyAnswer(index, index2) {
-      return this.actualChapter.questions[index].answers[index2].correct === true
-        ? "green"
-        : "red";
+    verifiedAnswersColors(index, index2) {
+      return this.verifyAnswers(index, index2) ? "green" : "red";
     },
     seeAnswers() {
-      this.seeAnswersVar = true
-      this.disabledSelect = true
-      this.answersSeen = true
-      console.log('ceva')
+      this.seeAnswersVar = true;
+      this.disabledSelect = true;
+      this.answersSeen = true;
     },
     setShowResponses(index, index2) {
-      return  this.seeAnswersVar === true ? this.verifyAnswer(index, index2) : 'primary' 
+      return this.seeAnswersVar === true
+        ? this.verifiedAnswersColors(index, index2)
+        : "primary";
     }
   }
 };
 </script>
 
 <style>
-/* i {
-  color: #2196F3;
-} */
+.v-progress-circular {
+  margin: 1rem;
+}
 </style>
